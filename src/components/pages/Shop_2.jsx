03fa8d308eb0ad2a4c50121687adaa049692b5e3
Jenish <jenish.rutilant@gmail.com>
2023-03-30 18:02:15 +0530

    dfssd

--- src/components/pages/Shop_2.jsx
+++ src/components/pages/Shop_2.jsx
@@ -16,7 +16,7 @@ const Shop_2 = () => {
             <p className="collection">Accessories Collection</p>
             <p className="smart">SMART WRIST WATCHES</p>
             <p className="lorem">Fusce egestas elit eget lorem</p>
-            <input type="submit" value="Send Now" className='send' />
+            <input type="submit" value="Send Now" className="send_Header" />
           </div>
           <div className="inner-headset">
             <img
@@ -66,35 +66,60 @@ const Shop_2 = () => {
         </div>
       </section>
 
-      {/* <div className="container-fluid">
+      <div className="container-fluid">
         <div className="main_filter_div">
           <div className="col-md-4">
-            <div className="filter-top-nav">
-              <p className="filter">Filter</p>
-              <p className="clearall">Clear All</p>
+            <nav
+              className="filter-top-nav navbar-expand-lg navbar-LIGHT bg-LIGHT"
+              aria-label="Eighth navbar example"
+            >
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarsExample07"
+                aria-controls="navbarsExample07"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                {/* <span className="navbar-toggler-icon"></span> */}
+                <i className="fa-solid fa-bars"></i>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarsExample07">
+                <ul className="navbar-nav1 me-auto mb-2 mb-lg-0 navpadle text-center">
+                  <li className="nav-item navspace1">
+                    <p className="filter">Filter</p>
+                    <p className="clearall">Clear All</p>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+          
+          <div className="col-md-4">
+            <div className="side_nav">
+              <p className="sort-by">Sort By:</p>
+              <div class="menu">
+                <button class="toggle">Default Sorting</button>
+                <ul class="list">
+                  <li class="list-item">A to Z list View</li>
+                  <li class="list-item">Z to A list View</li>
+                  <li class="list-item">Low To Hight List View</li>
+                  <li class="list-item">Hight To Low List View</li>
+                </ul>
+              </div>
             </div>
           </div>
-            <div className="col-md-8">
-              <div className="side_nav">
-                <p className="sort-by">Sort By:</p>
-                <div class="menu">
-                  <button class="toggle">Default Sorting</button>
-                  <ul class="list">
-                    <li class="list-item">A to Z list View</li>
-                    <li class="list-item">Z to A list View</li>
-                    <li class="list-item">Low To Hight List View</li>
-                    <li class="list-item">Hight To Low List View</li>
-                  </ul>
-                </div>
-              </div>
-              <div className="end-div">
-                <p className="reset">Reset</p>
-                <BsFillGridFill />
-                <BsList />
-              </div>
+
+          <div className="col-md-4">
+            <div className="end-div">
+              <p className="reset">Reset</p>
+              <BsFillGridFill />
+              <BsList />
             </div>
+          </div>
         </div>
-      </div> */}
+      </div>
     </>
   );
 };
