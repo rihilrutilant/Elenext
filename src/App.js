e39bd96bc7175276df413597706f01cf1027eb52
Jenish <jenish.rutilant@gmail.com>
2023-03-30 10:14:29 +0530

    sd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contact from "./components/pages/Contact";
 import Footer from "./components/pages/Footer";
 import Home from "./components/pages/Home";
 import Navbar from "./components/pages/Navbar";
+import Shop_2 from "./components/pages/Shop_2";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop_2 />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </>
   );
