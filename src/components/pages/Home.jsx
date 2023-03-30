de82c671f1b2b88bcf4105844a56f5780f3bc6a3
umangmalaviya <umang.rutilant@gmail.com>
2023-03-30 17:59:36 +0530

    update - 20230330

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,6 +7,7 @@ import "slick-carousel/slick/slick-theme.css";
 import slide1 from "../Images/hero-slider-img1.svg"
 import slide2 from "../Images/hero-slider-img2.svg"
 import SHOP_SUB from "./Shop_Sub";
+import star from "../Images/star.svg"
 
 const Home = () => {
   const settings = {
@@ -83,7 +84,109 @@ const Home = () => {
         </div>
       </section>
 
-      <SHOP_SUB />
+      <section className="popular-product">
+        <div className="container-fluid">
+          <div className="popular-product-title">
+            <h3>Popular Product</h3>
+            <Link to="#">View All Product</Link>
+          </div>
+          <SHOP_SUB />
+        </div>
+      </section>
+
+      <section className="new_arrival">
+        <div className="container-fluid">
+          <div className="new_arrival_cnt">
+            <div className="shadesofstyle new_arrival_info">
+              <img src={require("../Images/shades.png")} alt="shades" />
+              <div className="shadesofstyle_cnt">
+                <h2>Shades Of Style</h2>
+                <h3>Complete Your Look With The prefect Pair</h3>
+              </div>
+            </div>
+            <div className="stayuptodate new_arrival_info">
+              <img src={require("../Images/smartwatch.png")} alt="smartwatch" />
+              <div className="stayuptodate_cnt">
+                <h2>Stay Up TO date</h2>
+                <h3>With Smart Watch</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="hot_deals">
+        <div className="container-fluid">
+          <div className="hot_deals_combo">
+            <div className="hot_deals_part">
+              <h2>Hot Deals Of The Day</h2>
+              <div className="hot_deals_info">
+                <div className="hot_deals_img">
+                  <img src={require("../Images/hot-deals.png")} alt="hot deal" />
+                </div>
+                <div className="hot_deals_cnt">
+                  <h3>Sony Bravia 108 cm</h3>
+                  <h4>$160.00 <span>$200.00</span></h4>
+                  <div className='review-part'>
+                    <img src={star} alt="star" />
+                    <span>(10 Reviews)</span>
+                  </div>
+                  <Link to="#" className="addtocart_btn">Add To cart</Link>
+                  <Link to="#" className="buynow_btn">Buy Now</Link>
+                  <ul>
+                    <li><h5>01 <span>Day</span></h5></li>
+                    <li><h5>02 <span>Hour</span></h5></li>
+                    <li><h5>41 <span>Min</span></h5></li>
+                    <li><h5>22 <span>Sec</span></h5></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="top_seller">
+              <h2>Top 20 Best Seller</h2>
+              <div className="top_seller_info">
+                <div className="top_seller_img">
+                  <img src={require("../Images/top-seller-img1.png")} alt="top seller img" />
+                </div>
+                <div className="top_seller_cnt">
+                  <h3>Macbook Pro 3</h3>
+                  <div className='review-part'>
+                    <img src={star} alt="star" />
+                    <span>(10 Reviews)</span>
+                  </div>
+                  <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+              </div>
+              <div className="top_seller_info">
+                <div className="top_seller_img">
+                  <img src={require("../Images/top-seller-img1.png")} alt="top seller img" />
+                </div>
+                <div className="top_seller_cnt">
+                  <h3>Macbook Pro 3</h3>
+                  <div className='review-part'>
+                    <img src={star} alt="star" />
+                    <span>(10 Reviews)</span>
+                  </div>
+                  <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+              </div>
+              <div className="top_seller_info">
+                <div className="top_seller_img">
+                  <img src={require("../Images/top-seller-img1.png")} alt="top seller img" />
+                </div>
+                <div className="top_seller_cnt">
+                  <h3>Macbook Pro 3</h3>
+                  <div className='review-part'>
+                    <img src={star} alt="star" />
+                    <span>(10 Reviews)</span>
+                  </div>
+                  <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
--- src/components/pages/Shop_Sub.jsx
+++ src/components/pages/Shop_Sub.jsx
@@ -1,111 +1,103 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import "../Styles/Shop_Sub.css"
 import star from "../Images/star.svg"
 
 const Shop_Sub = () => {
     return (
         <>
-            <section className="popular-product">
-                <div className="container-fluid">
-                    <div className="popular-product-title">
-                        <h3>Popular Product</h3>
-                        <Link to="#">View All Product</Link>
+            <div className="popular-cnt">
+                <div className='popular_pro_info'>
+                    <img src={require("../Images/popular-img1.png")} alt=" popular-img" className='popular-cnt-img' />
+                    <h4>Instax Mini 9</h4>
+                    <div className='review-part'>
+                        <img src={star} alt="star" />
+                        <span>(10 Reviews)</span>
                     </div>
-                    <div className="popular-cnt">
-                        <div className='popular_pro_info'>
-                            <img src={require("../Images/popular-img1.png")} alt=" popular-img" className='popular-cnt-img' />
-                            <h4>Instax Mini 9</h4>
-                            <div className='review-part'>
-                                <img src={star} alt="star" />
-                                <span>(10 Reviews)</span>
-                            </div>
-                            <h5>$45.00 <span>$50.00</span></h5>
-                        </div>
-                        <div className='popular_pro_info'>
-                            <img src={require("../Images/popular-img2.png")} alt=" popular-img" className='popular-cnt-img' />
-                            <h4>Alexis 31 Bag</h4>
-                            <div className='review-part'>
-                                <img src={star} alt="star" />
-                                <span>(50 Reviews)</span>
-                            </div>
-                            <h5>$45.00 <span>$50.00</span></h5>
-                        </div>
-                        <div className='popular_pro_info'>
-                            <img src={require("../Images/popular-img3.png")} alt=" popular-img" className='popular-cnt-img' />
-                            <h4>Sony WH-1000XM5</h4>
-                            <div className='review-part'>
-                                <img src={star} alt="star" />
-                                <span>(6 Reviews)</span>
-                            </div>
-                            <h5>$45.00 <span>$50.00</span></h5>
-                        </div>
-                        <div className='popular_pro_info'>
-                            <img src={require("../Images/popular-img4.png")} alt=" popular-img" className='popular-cnt-img' />
-                            <h4>Fire-Boltt Phoenix</h4>
-                            <div className='review-part'>
-                                <img src={star} alt="star" />
-                                <span>(8 Reviews)</span>
-                            </div>
-                            <h5>$45.00 <span>$50.00</span></h5>
-                        </div>
-                        <div className='popular_pro_info'>
-                            <img src={require("../Images/popular-img5.png")} alt=" popular-img" className='popular-cnt-img' />
-                            <h4>Noise Pulse Go Buzz</h4>
-                            <div className='review-part'>
-                                <img src={star} alt="star" />
-                                <span>(65 Reviews)</span>
-                            </div>
-                            <h5>$45.00 <span>$50.00</span></h5>
-                        </div>
-                        <div className='popular_pro_info'>
-                            <img src={require("../Images/popular-img6.png")} alt=" popular-img" className='popular-cnt-img' />
-                            <h4>Redmi 11 Prime 5G</h4>
-                            <div className='review-part'>
-                                <img src={star} alt="star" />
-                                <span>(10 Reviews)</span>
-                            </div>
-                            <h5>$45.00 <span>$50.00</span></h5>
-                        </div>
-                        <div className='popular_pro_info'>
-                            <img src={require("../Images/popular-img7.png")} alt=" popular-img" className='popular-cnt-img' />
-                            <h4>Sony Bravia 108 cm</h4>
-                            <div className='review-part'>
-                                <img src={star} alt="star" />
-                                <span>(10 Reviews)</span>
-                            </div>
-                            <h5>$45.00 <span>$50.00</span></h5>
-                        </div>
-                        <div className='popular_pro_info'>
-                            <img src={require("../Images/popular-img8.png")} alt=" popular-img" className='popular-cnt-img' />
-                            <h4>Logitech M235 Wireless</h4>
-                            <div className='review-part'>
-                                <img src={star} alt="star" />
-                                <span>(10 Reviews)</span>
-                            </div>
-                            <h5>$45.00 <span>$50.00</span></h5>
-                        </div>
-                        <div className='popular_pro_info'>
-                            <img src={require("../Images/popular-img9.png")} alt=" popular-img" className='popular-cnt-img' />
-                            <h4>Noise Newly Launched</h4>
-                            <div className='review-part'>
-                                <img src={star} alt="star" />
-                                <span>(10 Reviews)</span>
-                            </div>
-                            <h5>$45.00 <span>$50.00</span></h5>
-                        </div>
-                        <div className='popular_pro_info'>
-                            <img src={require("../Images/popular-img10.png")} alt=" popular-img" className='popular-cnt-img' />
-                            <h4>Dell Gaming G15 5525</h4>
-                            <div className='review-part'>
-                                <img src={star} alt="star" />
-                                <span>(10 Reviews)</span>
-                            </div>
-                            <h5>$45.00 <span>$50.00</span></h5>
-                        </div>
-                    </div>
+                    <h5>$45.00 <span>$50.00</span></h5>
                 </div>
-            </section>
+                <div className='popular_pro_info'>
+                    <img src={require("../Images/popular-img2.png")} alt=" popular-img" className='popular-cnt-img' />
+                    <h4>Alexis 31 Bag</h4>
+                    <div className='review-part'>
+                        <img src={star} alt="star" />
+                        <span>(50 Reviews)</span>
+                    </div>
+                    <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+                <div className='popular_pro_info'>
+                    <img src={require("../Images/popular-img3.png")} alt=" popular-img" className='popular-cnt-img' />
+                    <h4>Sony WH-1000XM5</h4>
+                    <div className='review-part'>
+                        <img src={star} alt="star" />
+                        <span>(6 Reviews)</span>
+                    </div>
+                    <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+                <div className='popular_pro_info'>
+                    <img src={require("../Images/popular-img4.png")} alt=" popular-img" className='popular-cnt-img' />
+                    <h4>Fire-Boltt Phoenix</h4>
+                    <div className='review-part'>
+                        <img src={star} alt="star" />
+                        <span>(8 Reviews)</span>
+                    </div>
+                    <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+                <div className='popular_pro_info'>
+                    <img src={require("../Images/popular-img5.png")} alt=" popular-img" className='popular-cnt-img' />
+                    <h4>Noise Pulse Go Buzz</h4>
+                    <div className='review-part'>
+                        <img src={star} alt="star" />
+                        <span>(65 Reviews)</span>
+                    </div>
+                    <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+                <div className='popular_pro_info'>
+                    <img src={require("../Images/popular-img6.png")} alt=" popular-img" className='popular-cnt-img' />
+                    <h4>Redmi 11 Prime 5G</h4>
+                    <div className='review-part'>
+                        <img src={star} alt="star" />
+                        <span>(10 Reviews)</span>
+                    </div>
+                    <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+                <div className='popular_pro_info'>
+                    <img src={require("../Images/popular-img7.png")} alt=" popular-img" className='popular-cnt-img' />
+                    <h4>Sony Bravia 108 cm</h4>
+                    <div className='review-part'>
+                        <img src={star} alt="star" />
+                        <span>(10 Reviews)</span>
+                    </div>
+                    <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+                <div className='popular_pro_info'>
+                    <img src={require("../Images/popular-img8.png")} alt=" popular-img" className='popular-cnt-img' />
+                    <h4>Logitech M235 Wireless</h4>
+                    <div className='review-part'>
+                        <img src={star} alt="star" />
+                        <span>(10 Reviews)</span>
+                    </div>
+                    <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+                <div className='popular_pro_info'>
+                    <img src={require("../Images/popular-img9.png")} alt=" popular-img" className='popular-cnt-img' />
+                    <h4>Noise Newly Launched</h4>
+                    <div className='review-part'>
+                        <img src={star} alt="star" />
+                        <span>(10 Reviews)</span>
+                    </div>
+                    <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+                <div className='popular_pro_info'>
+                    <img src={require("../Images/popular-img10.png")} alt=" popular-img" className='popular-cnt-img' />
+                    <h4>Dell Gaming G15 5525</h4>
+                    <div className='review-part'>
+                        <img src={star} alt="star" />
+                        <span>(10 Reviews)</span>
+                    </div>
+                    <h5>$45.00 <span>$50.00</span></h5>
+                </div>
+            </div>
         </>
     )
 }
