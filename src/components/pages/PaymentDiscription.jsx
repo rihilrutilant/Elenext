be23fe945d7a876738512b30cb4afa9529bb271d
rihilrutilant <rihilrutilant@gmail.com>
2023-04-05 15:34:38 +0530

    update li

--- src/components/pages/PaymentDiscription.jsx
+++ src/components/pages/PaymentDiscription.jsx
@@ -142,48 +142,191 @@ const PaymentDiscription = () => {
                         Yes. Elenext offers you the convenience of using your bank's Internet Banking service to make a payment towards your order. With this you can directly transfer funds from your bank account, while conducting a highly secure transaction.
                     </p>
 
-                    <p className="pd_ans" style={{padding:"20px 0"}}>
+                    <p className="pd_ans" style={{ padding: "20px 0" }}>
                         We accept payment through Internet Banking for the following banks:
                     </p>
 
                     <table className='pd_table'>
                         <tr>
-                            <td className='pd_tbl'>State Bank of India</td>
-                            <td className='pd_tbl'>ICICI Bank</td>
-                            <td className='pd_tbl'>HDFC Bank</td>
-                            <td className='pd_tbl'>Punjab National Bank</td>
-                        </tr>
-                        <tr>
+                            <td className='pd_tbl'>ABN Amro Bank</td>
                             <td className='pd_tbl'>Axis Bank</td>
-                            <td className='pd_tbl'>Corporation Bank</td>
+                            <td className='pd_tbl'>Allahabad Bank</td>
                             <td className='pd_tbl'>Andhra Bank</td>
-                            <td className='pd_tbl'>Bank of Baroda</td>
                         </tr>
                         <tr>
-                            <td className='pd_tbl'>Standard Chartered India</td>
-                            <td className='pd_tbl'>Union Bank of India</td>
+                            <td className='pd_tbl'>Bank of Bahrain and Kuwait</td>
+                            <td className='pd_tbl'>Bank of Baroda</td>
+                            <td className='pd_tbl'>Bank of India</td>
+                            <td className='pd_tbl'>Bank of Maharashtra</td>
+                        </tr>
+                        <tr>
+                            <td className='pd_tbl'>Bank of Rajasthan</td>
                             <td className='pd_tbl'>Central Bank of India</td>
                             <td className='pd_tbl'>Citibank</td>
+                            <td className='pd_tbl'>City Union Bank</td>
                         </tr>
                         <tr>
-                            <td className='pd_tbl'>Deutsche Bank</td>
-                            <td className='pd_tbl'>Dhanlakshmi Bank</td>
-                            <td className='pd_tbl'>IDBI Bank</td>
-                            <td className='pd_tbl'>Canara Bank</td>
-                        </tr>
-                        <tr>
-                            <td className='pd_tbl'>IndusInd Bank</td>
                             <td className='pd_tbl'>Federal Bank</td>
-                            <td className='pd_tbl'>Kotak Mahindra Bank</td>
-                            <td className='pd_tbl'>Karur Vysya Bank</td>
+                            <td className='pd_tbl'>HDFC Bank</td>
+                            <td className='pd_tbl'>ICICI Bank</td>
+                            <td className='pd_tbl'>IDBI Bank</td>
                         </tr>
                         <tr>
+                            <td className='pd_tbl'>ING Vysya Bank</td>
+                            <td className='pd_tbl'>Indian Overseas Bank</td>
+                            <td className='pd_tbl'>Indian Bank</td>
+                            <td className='pd_tbl'>IndusInd Bank</td>
+                        </tr>
+                        <tr>
+                            <td className='pd_tbl'>Jammu & Kashmir Bank</td>
+                            <td className='pd_tbl'>Karnataka Bank</td>
+                            <td className='pd_tbl'>Karur Vysya Bank</td>
+                            <td className='pd_tbl'>Kotak Mahindra Bank</td>
+                        </tr>
+                        <tr>
+                            <td className='pd_tbl'>Lakshmi Vilas Bank</td>
                             <td className='pd_tbl'>Oriental Bank of Commerce</td>
+                            <td className='pd_tbl'>Punjab National Bank</td>
+                            <td className='pd_tbl'>Royal Bank of Scotland</td>
+                        </tr>
+                        <tr>
                             <td className='pd_tbl'>South Indian Bank</td>
+                            <td className='pd_tbl'>Standard Chartered Bank</td>
+                            <td className='pd_tbl'>State Bank of Hyderabad</td>
+                            <td className='pd_tbl'>State Bank of India</td>
+                        </tr>
+                        <tr>
+                            <td className='pd_tbl'>State Bank of Mysore</td>
+                            <td className='pd_tbl'>State Bank of Travancore</td>
+                            <td className='pd_tbl'>Syndicate Bank</td>
+                            <td className='pd_tbl'>Tamilnad Mercantile Bank</td>
+                        </tr>
+                        <tr>
+                            <td className='pd_tbl'>Union Bank of India</td>
+                            <td className='pd_tbl'>United Bank of India</td>
                             <td className='pd_tbl'>Vijaya Bank</td>
-                            <td className='pd_tbl'></td>
+                            <td className='pd_tbl'>YES Bank</td>
                         </tr>
                     </table>
+
+                    <div className="pd_q">
+                        Can I make a credit/debit card or Internet Banking payment on Elenext through my mobile?
+                    </div>
+                    <p className="pd_ans">
+                        Yes, you can make credit card payments through the Elenext mobile site and application. Elenext uses 256-bit encryption technology to protect your card information while securely transmitting it to the secure and trusted payment gateways managed by leading banks.
+                    </p>
+                    <div className="pd_q">
+                        How does 'Instant Cashback' work?
+                    </div>
+                    <p className="pd_ans">
+                        The 'Cashback' offer is instant and exclusive to Elenext.com. You only pay the final price you see in your shopping cart.
+                    </p>
+                    <div className="pd_q">
+                        How do I place a Cash on Delivery (C-o-D) order?
+                    </div>
+                    <p className="pd_ans">
+                        All items that have the "Cash on Delivery Available" icon are valid for order by Cash on Delivery.
+                    </p>
+                    <p className="pd_ans">
+                        Add the item(s) to your cart and proceed to checkout. When prompted to choose a payment option, select "Pay By Cash on Delivery". Enter the CAPTCHA text as shown, for validation.
+                    </p>
+                    <p className="pd_ans">
+                        Once verified and confirmed, your order will be processed for shipment in the time specified, from the date of confirmation. You will be required to make a cash-only payment to our courier partner at the time of delivery of your order to complete the payment.
+                    </p>
+                    <p className="pd_ans" style={{ padding: "10px 0" }}>
+                        Terms & Conditions:
+                    </p>
+                    <ul className='pd_list'>
+                        <li className="pd_ans">The maximum order value for C-o-D is ₹50,000</li>
+                        <li className="pd_ans">Gift Cards or Store Credit cannot be used for C-o-D orders</li>
+                        <li className="pd_ans">Cash-only payment at the time of delivery.</li>
+                    </ul>
+
+                    <div className="pd_q">
+                        What is Elenext's credit card EMI option?
+                    </div>
+                    <p className="pd_ans">
+                        With Elenext's credit card EMI option, you can choose to pay in easy installments of 3, 6, 9, 12, 18*, or 24 months* with credit cards from the following banks:
+                    </p>
+                    <ul className='pd_list'>
+                        <li className="pd_ans">HDFC</li>
+                        <li className="pd_ans">Citi</li>
+                        <li className="pd_ans">ICICI</li>
+                        <li className="pd_ans">Kotak</li>
+                        <li className="pd_ans">Axis</li>
+                        <li className="pd_ans">IndusInd</li>
+                        <li className="pd_ans">SBI</li>
+                        <li className="pd_ans">Standard Chartered</li>
+                        <li className="pd_ans">HSBC</li>
+                    </ul>
+                    <p className="pd_ans">
+                        *18 & 24 months EMI options are available from select banks only. Please refer to the table below for more details:
+                    </p>
+                    <table className='pd_table'>
+                        <tr>
+                            <td className='pd_tbl1'><strong>Banks</strong></td>
+                            <td className='pd_tbl2'><strong>Axis Bank</strong></td>
+                            <td className='pd_tbl3'><strong>Allahabad Bank</strong></td>
+                        </tr>
+                        <tr>
+                            <td className="pd_tbl1">HDFC</td>
+                            <td className="pd_tbl2">Yes</td>
+                            <td className="pd_tbl3">₹ 10,000</td>
+                        </tr>
+                        <tr>
+                            <td className="pd_tbl1">Citi</td>
+                            <td className="pd_tbl2">Yes</td>
+                            <td className="pd_tbl3">₹ 10,000</td>
+                        </tr>
+                        <tr>
+                            <td className="pd_tbl1">ICICI</td>
+                            <td className="pd_tbl2">Yes</td>
+                            <td className="pd_tbl3">₹ 10,000</td>
+                        </tr>
+                        <tr>
+                            <td className="pd_tbl1">Kotak</td>
+                            <td className="pd_tbl2">Yes</td>
+                            <td className="pd_tbl3">₹ 4,000</td>
+                        </tr>
+                        <tr>
+                            <td className="pd_tbl1">Axis</td>
+                            <td className="pd_tbl2">Yes</td>
+                            <td className="pd_tbl3">₹ 4,000</td>
+                        </tr>
+                        <tr>
+                            <td className="pd_tbl1">IndusInd</td>
+                            <td className="pd_tbl2">Yes</td>
+                            <td className="pd_tbl3">₹ 4,000</td>
+                        </tr>
+                        <tr>
+                            <td className="pd_tbl1">SBI</td>
+                            <td className="pd_tbl2">Yes</td>
+                            <td className="pd_tbl3">NA</td>
+                        </tr>
+                        <tr>
+                            <td className="pd_tbl1">Standard Chartered</td>
+                            <td className="pd_tbl2">Yes</td>
+                            <td className="pd_tbl3">₹ 4,000</td>
+                        </tr>
+                        <tr>
+                            <td className="pd_tbl1">HSBC</td>
+                            <td className="pd_tbl2">Yes</td>
+                            <td className="pd_tbl3">NA</td>
+                        </tr>
+                    </table>
+
+                    <p className="pd_ans">
+                        There is NO processing fee charged for availing Elenext's EMI payment option. On return or exchange, interest charged by the bank till that time will not be refunded by Elenext.
+                    </p>
+                    <p className="pd_ans">
+                        You may check with the respective bank/issuer on how a cancellation, refund or pre-closure could affect the EMI terms, and what interest charges would be levied on you for the same.
+                    </p>
+                    <div className="pd_q">
+                        Example and Calculations
+                    </div>
+                    <p className="pd_ans">
+                        The table below shows a representative rendering of EMI plans for a Rs 20,000 purchase on Elenext paid using the EMI payment plan
+                    </p>
                 </div>
             </div>
         </>
