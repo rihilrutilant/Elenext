3338fa4aa4e3020e2e1b9eda7e7d9c928e222192
Jenish <jenish.rutilant@gmail.com>
2023-04-06 13:00:16 +0530

    sdf

--- src/components/pages/ReturnPolicy.jsx
+++ src/components/pages/ReturnPolicy.jsx
@@ -23,10 +23,14 @@ const ReturnPolicy = () => {
           <br />
           <br /> Part 1 – Category, Return Window and Actions possible
         </p>
+        <br />
+        <br />
         <table className="rreturnpolicy">
           <tr className="rreturnpolicy">
-            <th className="rreturnpolicyth">Category</th>
-            <th className="rreturnpolicyth">
+            <th className="rreturnpolicyth" style={{ width: "25%" }}>
+              Category
+            </th>
+            <th className="rreturnpolicyth " style={{ width: "75%" }}>
               Returns Window, Actions Possible and Conditions (if any)
             </th>
           </tr>
@@ -118,31 +122,51 @@ const ReturnPolicy = () => {
           </tr>
           <tr>
             <td className="rreturnpolicytd">
-              Books (All books) Sports Equipments (Racquet, ball, support,
-              gloves, bags etc.) Exercise & Fitness Equipments (Home Gym combos,
-              dumbbell etc.) Auto Accessories - Car and Bike accessories
-              (helmets, car kit, media players etc.)
+              Books (All books) <br />
+              <br />
+              Sports Equipments (Racquet, ball, support, gloves, bags etc.)
+              <br />
+              <br />
+              Exercise & Fitness Equipments (Home Gym combos, dumbbell etc.)
+              <br />
+              <br />
+              Auto Accessories - Car and Bike accessories (helmets, car kit,
+              media players etc.)
             </td>
             <td className="rreturnpolicy2td">
-              7 days Replacement only Free replacement will be provided within 7
-              days if the product is delivered in defective/damaged condition or
-              different from the ordered item. Please keep the product intact,
-              with original accessories, user manual and warranty cards in the
-              original packaging at the time of returning the product.
+              7 days Replacement only <br />
+              <br />
+              Free replacement will be provided within 7 days if the product is
+              delivered in defective/damaged condition or different from the
+              ordered item.
+              <br />
+              <br /> Please keep the product intact, with original accessories,
+              user manual and warranty cards in the original packaging at the
+              time of returning the product.
             </td>
           </tr>
           <tr>
             <td className="rreturnpolicytd">
               Toys (Remote controlled toys, Learning toys, Stuffed toys etc.)
-              Stationary (Pens, Diary notebooks, Calculators etc.) Musical
-              Instruments (Microphones & Accessories, Guitars, Violins etc.)
+              <br />
+              <br />
+              Stationary (Pens, Diary notebooks, Calculators etc.)
+              <br />
+              <br /> Musical Instruments (Microphones & Accessories, Guitars,
+              Violins etc.)
             </td>
             <td className="rreturnpolicy2td">
-              7 days Replacement only Free replacement will be provided within 7
-              days if the product is delivered in defective/damaged condition or
-              different from the ordered item. Please keep the product intact,
-              with original accessories, user manual and warranty cards in the
-              original packaging at the time of returning the product.
+              7 days Replacement only
+              <br />
+              <br /> Free replacement will be provided within 7 days if the
+              product is delivered in defective/damaged condition or different
+              from the ordered item.
+              <br />
+              <br /> Please keep the product intact, with original accessories,
+              user manual and warranty cards in the original packaging at the
+              time of returning the product.
+              <br />
+              <br />
               Non-Returnable - All Wind Instruments (Harmonicas, Flutes etc.)
               This item is non-returnable due to hygiene and personal
               wellness.In case these products are delivered in damaged/defective
@@ -153,45 +177,265 @@ const ReturnPolicy = () => {
           <tr>
             <td className="rreturnpolicytd">
               All Mobiles (except Apple, Google, Motorola, Infinix, Redmi, MI,
-              Vivo, POCO, Realme, Samsung phones), Electronics - (except Apple /
-              Beats, Google, Realme, Samsung, JBL& Infinity, Epson, HP, Dell,
-              Canon, MI, Dizo Products (Tablets, Laptops, Smart Watches) All
-              Small Home Appliances (Except Chimney, Water Purifier, Fan,
-              Geyser) Furniture - Hammock Swing & Stool
+              Vivo, POCO, Realme, Samsung phones),
+              <br />
+              <br /> Electronics - (except Apple / Beats, Google, Realme,
+              Samsung, JBL& Infinity, Epson, HP, Dell, Canon, MI, Dizo Products
+              (Tablets, Laptops, Smart Watches)
+              <br />
+              <br /> All Small Home Appliances (Except Chimney, Water Purifier,
+              Fan, Geyser)
+              <br />
+              <br /> Furniture - Hammock Swing & Stool
             </td>
             <td className="rreturnpolicy2td">
-              7 days Replacement only In order to help you resolve issues with
-              your product, we may troubleshoot your product either through
-              online tools, over the phone, and/or through an in-person
-              technical visit. If a defect is determined within the Returns
-              Window, a replacement of the same model will be provided at no
-              additional cost. If no defect is confirmed or the issue is not
-              diagnosed within 7 days of delivery, you will be directed to a
-              brand service center to resolve any subsequent issues. In any
-              case, only one replacement shall be provided.
+              7 days <br />
+              <br />
+              Replacement only <br />
+              <br />
+              In order to help you resolve issues with your product, we may
+              troubleshoot your product either through online tools, over the
+              phone, and/or through an in-person technical visit. <br />
+              <br />
+              If a defect is determined within the Returns Window, a replacement
+              of the same model will be provided at no additional cost. If no
+              defect is confirmed or the issue is not diagnosed within 7 days of
+              delivery, you will be directed to a brand service center to
+              resolve any subsequent issues. <br />
+              <br />
+              In any case, only one replacement shall be provided.
             </td>
           </tr>
           <tr>
             <td className="rreturnpolicytd">
               Mobile – Apple, Google, Motorola, Infinix, Redmi, MI, Vivo, POCO,
-              Realme, Samsung phones Electronics - Apple / Beats, Google,
-              Realme, Samsung, JBL & Infinity, Epson, HP, Dell, Canon, Dizo & MI
-              Products (Tablets, Laptops, Smart Watches) Large –Vu, LG, Godrej,
-              Haier, IFB, Hindware , Glen, Faber, AGARO, Voltas, BOSCH
+              Realme, Samsung phones
+              <br />
+              <br /> Electronics - Apple / Beats, Google, Realme, Samsung, JBL &
+              Infinity, Epson, HP, Dell, Canon, Dizo & MI Products (Tablets,
+              Laptops, Smart Watches)
+              <br />
+              <br /> Large –Vu, LG, Godrej, Haier, IFB, Hindware , Glen, Faber,
+              AGARO, Voltas, BOSCH
             </td>
             <td className="rreturnpolicy2td">
-              7 Days Service Center Replacement/Repair Replacement only Brand
-              assistance for device related issues is subject to brand warranty
-              guidelines and service policies. Please reach out to the nearest
-              brand authorized service centre for more detail Please note that
-              Elenext is an online marketplace and the final decision on
-              replacement of defective device rests with the seller/brand
-              Authorized Service partner Locator for Brands: Click here For any
-              other issues with the product, you may contact Elenext - Elenext’s
-              24×7 Customer Care
+              7 Days Service Center Replacement/Repair
+              <br />
+              <br /> Replacement only <br />
+              <br />
+              Brand assistance for device related issues is subject to brand
+              warranty guidelines and service policies.
+              <br />
+              <br /> Please reach out to the nearest brand authorized service
+              centre for more detail Please note that Elenext is an online
+              marketplace and the final decision on replacement of defective
+              device rests with the seller/brand
+              <br />
+              <br />
+              Authorized Service partner Locator for Brands: Click here
+              <br />
+              <br /> For any other issues with the product, you may contact
+              Elenext - Elenext’s 24×7 Customer Care
+            </td>
+          </tr>
+          <tr>
+            <td className="rreturnpolicytd">
+              Furniture, Large appliances ( Except Vu, LG, Godrej, Haier, IFB,
+              Hindware , Glen, Faber, AGARO, Voltas, BOSCH )<br />
+              <br /> Rest of Small Home Appliances -Chimney, Water Purifier,
+              Fan, Geyser only
+            </td>
+            <td className="rreturnpolicy2td">
+              10 days
+              <br />
+              <br /> Replacement only
+              <br />
+              <br /> For products requiring installation, returns shall be
+              eligible only when such products are installed by the brand's
+              authorized personnel.
+              <br />
+              <br /> In order to help you resolve issues with your product, we
+              may troubleshoot your product either through online tools, over
+              the phone, and/or through an in-person technical visit.
+              <br />
+              <br /> If a defect is determined within the Returns Window, a
+              replacement of the same model will be provided at no additional
+              cost. If no defect is confirmed or the issue is not diagnosed
+              within 10 days of delivery or Installation wherever applicable,
+              you will be directed to a brand service centre to resolve any
+              subsequent issues. <br />
+              <br />
+              In any case, only one replacement shall be provided.
+            </td>
+          </tr>
+          <tr>
+            <td className="rreturnpolicytd">No Questions Asked</td>
+            <td className="rreturnpolicy2td">
+              10 days <br />
+              <br />
+              Refund or replacement <br />
+              <br />
+              This policy enables easy product return requests for customers
+              through the Platform, subject to product validations at the time
+              of pick-up and fraud prevention mechanisms.
+              <br />
+              <br /> This policy shall be applicable only if the product was
+              bought when this policy was applicable to the product. If not, the
+              policy provided here shall apply to the order. It is clarified
+              that a customer may only be able to seek a one-time replacement
+              under this Policy, subject to the other terms provided herein.
+              <br />
+              <br />
+              Exceptions to this policy: Following claims will be covered under
+              the policy provided here and through corresponding validation
+              processes
+              <br /> a. product undelivered <br />
+              b. product/accessories missing <br />
+              c. wrong product/accessories delivered
+            </td>
+          </tr>
+          <tr>
+            <td className="rreturnpolicytd">No Returns categories</td>
+            <td className="rreturnpolicy2td">
+              Some products in the above categories are not returnable due to
+              their nature or other reasons. For all products, the policy on the
+              product page shall prevail.
+              <br />
+              <br /> You can view the complete list of non-returnable products
+              here.
+            </td>
+          </tr>
+          <tr>
+            <td className="rreturnpolicytd">Refurbished</td>
+            <td className="rreturnpolicy2td">
+              7 days <br />
+              <br />
+              Replacement only <br />
+              <br />
+              To help you resolve issues with your product, we may troubleshoot
+              your product either through online tools, over the phone, and/or
+              through an in-person technical visit. <br />
+              <br />
+              If a defect is determined within the Returns Window, a replacement
+              of the same model will be provided at no additional cost. If no
+              defect is confirmed or the issue is not diagnosed within 7 days of
+              delivery, you will be directed to the warranty partner for
+              resolving any subsequent issues.
             </td>
           </tr>
         </table>
+        <br />
+        <br />
+        <p className="pd_ans">
+          Part 2 - Returns Pick-Up and Processing In case of returns where you
+          would like item(s) to be picked up from a different address, the
+          address can only be changed if pick-up service is available at the new
+          address During pick-up, your product will be checked for the following
+          conditions:
+        </p>
+        <br />
+        <br />
+        <table className="rreturnpolicy">
+          <tr className="rreturnpolicy">
+            <th className="rreturnpolicyth" style={{ width: "22%" }}>
+              Category
+            </th>
+            <th className="rreturnpolicyth" style={{ width: "78%" }}>
+              Conditions
+            </th>
+          </tr>
+          <tr>
+            <td className="rreturnpolicytd">Complete Product</td>
+            <td className="rreturnpolicy2td">
+              All in-the-box accessories (like remote control, starter kits,
+              instruction manuals, chargers, headphones, etc.), freebies and
+              combos (if any) should be present.
+            </td>
+          </tr>
+          <tr>
+            <td className="rreturnpolicytd">Unused Product</td>
+            <td className="rreturnpolicy2td">
+              The product should be unused, unwashed, unsoiled, without any
+              stains and with non-tampered quality check seals/return
+              tags/warranty seals (wherever applicable). Before returning a
+              Mobile/ Laptop/ Tablet, the device should be formatted and Screen
+              Lock (Pin, Pattern or Fingerprint) must be disabled. iCloud lock
+              must be disabled for Apple devices.
+            </td>
+          </tr>
+          <tr>
+            <td className="rreturnpolicytd">Correct Product</td>
+            <td className="rreturnpolicy2td">
+              IMEI/ name/ image/ brand/ serial number/ article number/ bar code
+              should match and MRP tag should be undetached and clearly visible.
+            </td>
+          </tr>
+          <tr>
+            <td className="rreturnpolicytd">Undamaged Product</td>
+            <td className="rreturnpolicy2td">
+              The product (including SIM trays/ charging port/ headphone port,
+              back-panel etc.) should be undamaged and without any scratches,
+              dents, tears or holes.
+            </td>
+          </tr>
+          <tr>
+            <td className="rreturnpolicytd">Undamaged Packaging</td>
+            <td className="rreturnpolicy2td">
+              The product’s original packaging/ box should be undamaged.
+            </td>
+          </tr>
+        </table>
+        <br />
+        <br />
+        <p className="pd_ans">
+          The field executive will refuse to accept the return if any of the
+          above conditions are not met. <br />
+          <br />
+          For any products for which a refund is to be given, the refund will be
+          processed once the returned product has been received by the seller.{" "}
+          <br />
+          <br />
+          Part 3 - General Rules for a successful Return
+        </p>
+        <ul className="pd_list2">
+          <li className="pd_ans">
+            Choose your credit-card issuing bank you wish to pay from
+          </li>
+          <li className="pd_ans">
+            In certain cases where the seller is unable to process a replacement
+            for any reason whatsoever, a refund will be given.
+          </li>
+          <li className="pd_ans">
+            In cases where a product accessory is found
+            missing/damaged/defective, the seller may either process a
+            replacement of the particular accessory or issue an eGV for an
+            amount equivalent to the price of the accessory, at the seller’s
+            discretion.
+          </li>
+          <li className="pd_ans">
+            During open box deliveries, while accepting your order, if you
+            received a different or a damaged product, you will be given a
+            refund (on the spot refunds for cash-on-delivery orders). Once you
+            have accepted an open box delivery, no return request will be
+            processed, except for manufacturing defects. In such cases, these
+            category-specific replacement/return general conditions will be
+            applicable. Click here to know more about Open Box Delivery
+          </li>
+          <li className="pd_ans">
+            For products where installation is provided by Elenext's service
+            partners, do not open the product packaging by yourself. Elenext
+            authorised personnel shall help in unboxing and installation of the
+            product.
+          </li>
+          <li className="pd_ans">
+            For Furniture, any product-related issues will be checked by
+            authorised service personnel (free of cost) and attempted to be
+            resolved by replacing the faulty/ defective part of the product.
+            Full replacement will be provided only in cases where the service
+            personnel opines that replacing the faulty/defective part will not
+            resolve the issue.'
+          </li>
+        </ul>
       </div>
     </div>
   );
