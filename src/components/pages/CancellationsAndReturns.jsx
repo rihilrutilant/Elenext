a4ed4c35765d58b00ce32affa804f9466129bb75
Jenish <jenish.rutilant@gmail.com>
2023-04-08 10:29:49 +0530

    sdf

--- src/components/pages/CancellationsAndReturns.jsx
+++ src/components/pages/CancellationsAndReturns.jsx
@@ -1,19 +1,304 @@
 import React, { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
 import "../Styles/CancellationsAndReturns.css";
 
 const CancellationsAndReturns = () => {
 
-  let location = useLocation()
-  const [Nav_toggle, setNav_toggle] = useState(false)
+  const [Nav_toggle, setNav_toggle] = useState(false);
+  const [Nav_toggle2, setNav_toggle2] = useState(false);
+  const [Nav_toggle3, setNav_toggle3] = useState(false);
+  const [Nav_toggle4, setNav_toggle4] = useState(false);
+  const [Nav_toggle5, setNav_toggle5] = useState(false);
+  const [Nav_toggle6, setNav_toggle6] = useState(false);
+  const [Nav_toggle7, setNav_toggle7] = useState(false);
+  const [Nav_toggle8, setNav_toggle8] = useState(false);
+  const [Nav_toggle9, setNav_toggle9] = useState(false);
+  const [Nav_toggle10, setNav_toggle10] = useState(false);
+  const [Nav_toggle11, setNav_toggle11] = useState(false);
+  const [Nav_toggle12, setNav_toggle12] = useState(false);
+  const [Nav_toggle13, setNav_toggle13] = useState(false);
+  const [Nav_toggle14, setNav_toggle14] = useState(false);
+
   const toggle_nav = () => {
     if (Nav_toggle === false) {
-      setNav_toggle(true)
+      setNav_toggle(true);
+      setNav_toggle2(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle(false);
     }
-    else {
-      setNav_toggle(false)
+  };
+
+  const toggle_nav2 = () => {
+    if (Nav_toggle2 === false) {
+      setNav_toggle2(true);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle2(false);
     }
-  }
+  };
+  const toggle_nav3 = () => {
+    if (Nav_toggle3 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(true);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle3(false);
+    }
+  };
+  const toggle_nav4 = () => {
+    if (Nav_toggle4 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(true);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle4(false);
+    }
+  };
+  const toggle_nav5 = () => {
+    if (Nav_toggle5 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(true);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle5(false);
+    }
+  };
+  const toggle_nav6 = () => {
+    if (Nav_toggle6 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(true);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle6(false);
+    }
+  };
+  const toggle_nav7 = () => {
+    if (Nav_toggle7 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(true);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle7(false);
+    }
+  };
+  const toggle_nav8 = () => {
+    if (Nav_toggle8 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(true);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle8(false);
+    }
+  };
+  const toggle_nav9 = () => {
+    if (Nav_toggle9 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(true);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle9(false);
+    }
+  };
+  const toggle_nav10 = () => {
+    if (Nav_toggle10 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(true);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle10(false);
+    }
+  };
+  const toggle_nav11 = () => {
+    if (Nav_toggle11 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(true);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle11(false);
+    }
+  };
+  const toggle_nav12 = () => {
+    if (Nav_toggle12 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(true);
+      setNav_toggle13(false);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle12(false);
+    }
+  };
+  const toggle_nav13 = () => {
+    if (Nav_toggle13 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(true);
+      setNav_toggle14(false);
+    } else {
+      setNav_toggle13(false);
+    }
+  };
+  const toggle_nav14 = () => {
+    if (Nav_toggle14 === false) {
+      setNav_toggle2(false);
+      setNav_toggle(false);
+      setNav_toggle3(false);
+      setNav_toggle4(false);
+      setNav_toggle5(false);
+      setNav_toggle6(false);
+      setNav_toggle7(false);
+      setNav_toggle8(false);
+      setNav_toggle9(false);
+      setNav_toggle10(false);
+      setNav_toggle11(false);
+      setNav_toggle12(false);
+      setNav_toggle13(false);
+      setNav_toggle14(true);
+    } else {
+      setNav_toggle14(false);
+    }
+  };
 
   return (
     <div className="container-fluid">
@@ -44,62 +329,149 @@ const CancellationsAndReturns = () => {
         <br />
 
         <div className="main-inner-content">
-          <div className="type-of-issue">
-            <h6>Type Of Issue</h6>
+          <div className="types-of-issue">
+            <h6 className="type_of_issue">Type Of Issue</h6>
             <hr className="hr123" />
-            <ul>
-              <li>
-                <Link onClick={toggle_nav} className={`${location.pathname === '/CancellationsAndReturns' ? "active" : " "}`} to="/CancellationsAndReturns"> Help with your Issues</Link>
-              </li>
-              <li>
-                <Link>Help with your order</Link>
-              </li>
-              <li>
-                <Link>Help with other Issues</Link>
+            <ul className="main_sidebar_upper_ul">
+              <li className="main_sidebar_upper_li">
+                <div
+                  onClick={toggle_nav}
+                  className={`${
+                    Nav_toggle ? "active123123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >
+                  {" "}
+                  Help with your Issues
+                </div>
               </li>
             </ul>
             <hr className="hr123" />
-            <h6>Type Of Issue</h6>
+            <h6 className="type_of_issue">Type Of Issue</h6>
             <hr className="hr123" />
-            <ul>
-              <li>
-                <Link>Order</Link>
+            <ul className="main_sidebar_upper_ul">
+              <li className="main_sidebar_upper_li">
+                <div
+                  onClick={toggle_nav2}
+                  className={`${
+                    Nav_toggle2 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >
+                  Order
+                </div>
               </li>
               <li>
-                <Link>Cancellations and Returns</Link>
+                <div
+                  onClick={toggle_nav3}
+                  className={`${
+                    Nav_toggle3 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >
+                  Cancellations and Returns
+                </div>
               </li>
               <li>
-                <Link>Payment</Link>
+                <div
+                  onClick={toggle_nav4}
+                  className={`${
+                    Nav_toggle4 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >Payment</div>
               </li>
               <li>
-                <Link>Shopping</Link>
+                <div
+                  onClick={toggle_nav5 }
+                  className={`${
+                    Nav_toggle5 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >Shopping</div>
               </li>
               <li>
-                <Link>Wallet</Link>
+                <div
+                  onClick={toggle_nav6}
+                  className={`${
+                    Nav_toggle6 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >Wallet</div>
               </li>
               <li>
-                <Link>Others</Link>
+                <div
+                  onClick={toggle_nav7}
+                  className={`${
+                    Nav_toggle7 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >Others</div>
               </li>
               <li>
-                <Link>Insurance</Link>
+                <div
+                  onClick={toggle_nav8}
+                  className={`${
+                    Nav_toggle8 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >Insurance</div>
               </li>
               <li>
-                <Link>Elenext Quick</Link>
+                <div
+                  onClick={toggle_nav9}
+                  className={`${
+                    Nav_toggle9 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >Elenext Quick</div>
               </li>
               <li>
-                <Link>SuperCoins</Link>
+                <div
+                  onClick={toggle_nav10}
+                  className={`${
+                    Nav_toggle10 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >SuperCoins</div>
               </li>
               <li>
-                <Link>Refurbished</Link>
+                <div
+                  onClick={toggle_nav11}
+                  className={`${
+                    Nav_toggle11 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >Refurbished</div>
               </li>
               <li>
-                <Link>Elenext Travel</Link>
+                <div
+                  onClick={toggle_nav12}
+                  className={`${
+                    Nav_toggle12 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >Elenext Travel</div>
               </li>
               <li>
-                <Link>Electric Vehicle</Link>
+                <div
+                  onClick={toggle_nav13}
+                  className={`${
+                    Nav_toggle13 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >
+                  Electric Vehicle
+                </div>
               </li>
               <li>
-                <Link>Elenext Plus</Link>
+                <div
+                  onClick={toggle_nav14}
+                  className={`${
+                    Nav_toggle14 ? "active123" : "main_sidebar_upper_li_div"
+                  }`}
+                  to="/CancellationsAndReturns"
+                >Elenext Plus</div>
               </li>
             </ul>
           </div>
