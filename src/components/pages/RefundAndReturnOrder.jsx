9e665f031f3ee26abce7232acb72f8d962aeab3b
Jenish <jenish.rutilant@gmail.com>
2023-04-26 14:41:51 +0530

    gdf

--- src/components/pages/RefundAndReturnOrder.jsx
+++ src/components/pages/RefundAndReturnOrder.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import "../Styles/RefundAndReturnOrder.css"
 
 const RefundAndReturnOrder = () => {
     return (
@@ -9,7 +10,7 @@ const RefundAndReturnOrder = () => {
                     <Link className="home_hover" to="/">
                         Home
                     </Link>
-                    &nbsp;&gt; Contact Us
+                    &nbsp;&gt;  Cancel Order
                 </p>
                 <div className="main_banner">
                     <div className="inner_text">
@@ -25,6 +26,54 @@ const RefundAndReturnOrder = () => {
                             alt=" "
                         />
                     </div>
+                </div>
+
+                <div className="main-randrorder">
+                    <div className="left-side-randr">
+                        <div className="uperside-left">
+                            <div className="order_placed">
+                                <p>ORDER PLACED</p>
+                                <span>18 March 2023</span>
+                            </div>
+                            <div className="total">
+                                <p>TOTAL </p>
+                                <span>₹1,699.00</span>
+                            </div>
+                            <div className="ship">
+                                <p>SHIP TO</p>
+                                <span style={{ color: "#0E4DC5", cursor: "pointer" }}>Rutilant Technology</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right-side-randr">
+                        <p>ORDER # 404-9197942-3768339</p>
+                        <span>View order details | Invoice</span>
+                    </div>
+                </div>
+
+                <div className="middle-part">
+                    <p>Return Product</p>
+                    <h6>Your return is complete.&nbsp;<span>When will I get my refund?</span></h6>
+
+                    <div className="main-middle-part">
+                        <div className="inner-middle-part">
+                            <img src={require("../Images/cart_1.png")} alt=" " />
+                            <p>Noise Pulse Go Buzz Smart Watch with Latest Bluetooth Calling tech, 1.69" Clear Display, 550Nits, 150+ Watch face, Comfort Strap, Heart Rate, Steps & Sleep Tracker, IP68, 7 Days Battery(Mist Grey)</p>
+                        </div>
+
+                        <div className="end-right-side">
+                            <Link to='/WishList' className='track-parage'>Track package</Link>
+                            <Link className='cancel-items'>Cancel items</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="arachive">
+                    <Link to='/Product'>
+                        <p>
+                            Archive order
+                        </p>
+                    </Link>
                 </div>
             </div>
         </>
