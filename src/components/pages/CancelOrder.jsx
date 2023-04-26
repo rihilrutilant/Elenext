4e7ba951a10be0674adc5c67cdbaaf3434e0dc4c
Jenish <jenish.rutilant@gmail.com>
2023-04-26 17:54:33 +0530

    jg

--- src/components/pages/CancelOrder.jsx
+++ src/components/pages/CancelOrder.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Styles/CancelOrder.css"
 import { Link } from "react-router-dom";
 import choose from "../Images/choose.svg"
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const CancelOrder = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <>
             <div className="container-fluid">
@@ -40,6 +47,48 @@ const CancelOrder = () => {
                             <p>$179.00</p>
                         </div>
                     </div>
+
+                    <select name="" id="">
+                        <option disabled selected value="">Select Cancellation Reason</option>
+                        <option value="">Order Created by Mistake</option>
+                        <option value="">Item(s) Would Not Arrive on Time</option>
+                        <option value="">Shipping Cost Too High</option>
+                        <option value="">Item Price Too High</option>
+                        <option value="">Found Cheaper Somewhere Else</option>
+                        <option value="">Need to Change Shipping Address</option>
+                        <option value="">Need to Change Billing Address</option>
+                        <option value="">Need to Change Payment Method</option>
+                        <option value="">Need to Change Shipping Speed</option>
+                        <option value="">Other</option>
+                    </select>
+
+                    <div className="comments">
+                        <p>Comments<span style={{ color: "red" }}>*</span></p>
+                        <textarea name="" id="" rows="7"></textarea>
+                        <p className='continue' onClick={handleShow}>Continue</p>
+                    </div>
+
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Confirm Cancellation</Modal.Title>
+                        </Modal.Header>
+                        <div className="display-flex-body">
+                            <img src={require("../Images/discount.png")} alt=" " />
+                            <Modal.Body>You saved ₹2200 on this product!</Modal.Body>
+                            <img src={require("../Images/cart_1.png")} alt=" " className='headset_popop'/>
+                        </div>
+                        <div className="down-body">
+                            <Modal.Body>If you cancel now, you may not be able to avail this deal again. Do you still want to cancel?</Modal.Body>
+                        </div>
+                        <Modal.Footer>
+                            <Button style={{background: "#FFFFFF", color: "#000000"}} onClick={handleClose} className="inner-modal-footer">
+                                Cancel Order
+                            </Button>
+                            <Button style={{background: "#FEA700", color: "#FFFFFF"}} onClick={handleClose} className="inner-modal-footer">
+                                Don't cancel
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </div>
             </div>
         </>
